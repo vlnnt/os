5d6b6be376a1ccbc8356e697c2eb09d6f6546740
vlnnt <maksolb@maksolb>
2022-06-24 21:00:18 +0300

    kernel update. add clear and print text functions.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,29 @@
-void enterpnt() {}
+static int point_x = 0;
+static int point_y = 0;
+static int fill_column_area = 80;
+static int fill_line_area = 25;
+static int each_fill_element = 2;
 
-void main() {
+void __clearscreen(char* videomem) {
+  while(point_y < fill_column_area * fill_line_area * each_fill_element) {
+    videomem[point_y] = ' ';
+    videomem[point_y+1] = 0x04;
+    point_y += 2;
+  }   
+}
 
-    char *videomem[256] = (char*)0xb8000;
-    *videomem = 'Welcome to operating system!';
+void __printmsg(const char* msg, char* videomem) {
+   while(msg[point_y] != '\0') {
+     videomem[point_x] = msg[point_y];
+     videomem[point_x+1] = 0x04;
+     ++point_y;
+     point_x += 2;
+   }
+}
+
+void main(void) {
+   const char *welcome_msg = "Welcome to Operating System!";
+   char *videomem = (char*)0xb8000;
+   __clearscreen(videomem);
+   __printmsg(welcome_msg, videomem);
 }
